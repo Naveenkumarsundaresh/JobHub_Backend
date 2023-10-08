e6d64213d406eee5ad3a3b2ad9f0b70740b60b8a
Nobita0209 <138775980+Nobita0209@users.noreply.github.com>
2023-10-08 23:03:57 +0530

    new index.js

--- index.js
+++ index.js
@@ -31,4 +31,75 @@ app.use("/api/messages", messageRoute);
 
 app.listen(process.env.PORT || 4000, () => console.log(`Example app listening on port ${process.env.PORT}!`));
 
+const io = require('socket.io')(server,{
+pingTimeout:60000,
+cors:{
+    //Hosted Server
+    origin:"https://jobhubrest-production-f317.up.railway.app/"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("connected to sockets");
+
+    socket.on('setup', (userId) => {
+        socket.join(userId);
+        socket.broadcast.emit('online-user', userId)
+        console.log(userId);
+    });
+
+
+    socket.on('typing', (room) => {
+        // console.log("typing");
+        // console.log("room");
+        socket.to(room).emit('typing', room)
+    });
+
+
+    socket.on('stop typing', (room) => {
+        // console.log("stop typing");
+        // console.log("room");
+        socket.to(room).emit('stop typing', room)
+    });
+
+
+    socket.on('join chat', (room) => {
+        socket.join(room)
+        // console.log('User Joined : ' + room);
+    });
+
+    socket.on('new message', (newMessageReceived) => {
+
+        var chat = newMessageReceived.chat;
+        var room = chat._id;
+
+        var sender = newMessageReceived.sender;
+
+        if (!sender || sender._id) {
+            // console.log('Sender not defined');
+            return;
+        }
+
+        var senderId = sender._id;
+        // console.log(senderId + "message sender");
+        const users = chat.users;
+
+        if (!users) {
+            // console.log(" Users not defined");
+            return;
+        }
+
+        socket.to(room).emit('message received', newMessageReceived);
+        socket.to(room).emit('message sent', "New Message");
+    });
+
+
+    socket.off('setup', () => {
+        // console.log('user offline');
+        socket.leave(userId)
+    })
+
+
+})
+
 
